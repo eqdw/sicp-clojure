4112e4245fccfd65fbbf7e7126d7fb06617d877f
erauqssi <erauqssidlroweht@eqdw.net>
2009-01-27 10:34:40 -0600

    fixed a minor potential issue

--- chpt1/1.1.11.clj
+++ chpt1/1.1.11.clj
@@ -6,20 +6,13 @@
   (+ (recursive-f (- n 1)) (* 2 (recursive-f (- n 2))) (* 3 (recursive-f (- n 3))))))
 
 
-
 (defn iter-f [n]
   (if (< n 3)
     n
     (f-iter n 3 2 1 0)))
 
 (defn f-iter [n i fn-1 fn-2 fn-3]
-  (let [fn (+ fn-1 (* 2 fn-2) (* 3 fn-3))]
+  (let [f (+ fn-1 (* 2 fn-2) (* 3 fn-3))]
     (if (= n i)
-      fn
-      (f-iter n (+ i 1) fn fn-1 fn-2))))
-
-
-
-
-
-  
+      f
+      (f-iter n (+ i 1) f fn-1 fn-2))))
