ab3c97cf87e24ab47989d9cc1a2c35afc5da1bf3
erauqssi <erauqssidlroweht@eqdw.net>
2009-01-30 10:00:44 -0600

    SICP Problem 1.1.16

--- chpt1/1.1.12.clj
+++ chpt1/1.1.12.clj
@@ -1,13 +1,17 @@
 (ns sicp.chpt1)
 
-;;defines an infinite sequence. Lazy eval to the rescue! Also, either I don't understand this
-;;question properly, or SICP expects us to have absorved Cons from the air or something
 
-(defn pascals-triangle
-  (triangle-recurse '(1)))
+(defn -- [x]
+  (- x 1))
 
-;;Recursively generates a list of lists, where each internal list
-;;contains a row of Pascal's Triangle
-(defn triangle-recurse [tri]
-  ;;in The Little Schemer terms, this would appear to be a *-method. And it's late. :(
-  )
+
+
+;;Treats Pascal's Triangle as a function of two arguments n and r, where
+;;n is the row and r is the column of the value. Ie, p(4, 2) == 3
+
+(defn triangle [n r]
+  (if (or (= r 1) (= n r))
+    1
+    (+ (triangle (-- n) (-- r)) (triangle (-- n) r))))
+
+
--- chpt1/1.1.15.clj
+++ chpt1/1.1.15.clj
@@ -5,13 +5,35 @@
     (- 0 x)
     x))
 
+
 (defn cube [x]
   (* x x x))
 
 (defn p [x]
+  (println "p executed")
   (- (* 3 x) (* 4 (cube x))))
+
 
 (defn sine [angle]
   (if (not (> (abs angle) 0.1))
-    angle
+    (angle)
     (p (sine (/ angle 3.0)))))
+
+#'user/sine
+
+(sine 12.15)
+
+;;==>
+;; p executed
+;; p executed
+;; p executed
+;; p executed
+;; p executed
+;; -0.39980345741334
+
+;;It appears to have been executed 5 times.
+
+
+
+;;A cursory program trace seems to suggest that this sine approximation algorithm is
+;;order O(n*lg n) in time and O(lg n) in space
